6a519bd678cf7cbae62db1cd948fb588293cd5d8
Jared Williams <jared@Jareds-iMac.local>
2013-10-29 12:28:31 +0800

    Small bug fixes
    Fixed script running for AJAX requests, fixed (again) wrong namespace

--- Newsola Filter.tamper.js
+++ Newsola Filter.tamper.js
@@ -1,16 +1,18 @@
 // ==UserScript==
-// @name       Newsola Filter
-// @namespace  http://www.jaredwilliams.com
-// @version    0.1
-// @description  Filters headlines from Newsola to suit what you do or do not want to read.
-// @match      *newsola.com*
-// @copyright  2012+, You
-// ==/UserScript==
+// @name		Newsola Filter
+// @namespace	http://www.jaredwilliams.com.au
+// @version		0.1
+// @description	Filters headlines from Newsola to suit what you do or do not want to read.
+// @include		*newsola.com*
+// @exclude		*newsola.com/get*
+// @copyright	2012+, You
+// ==/UserScript==	
+
 
 console.log('Running Newsola Filter userscript');
-
+    
 $(document).ajaxComplete(function() {
-	var cells, badwords, badword, numremoved = 0;
+    var cells, badwords, badword, numremoved = 0;
     
     //Our list of words to remove headlines...
     badwords = ['dead', 'killed', 'murder', 'death', 'shoot', 'fire'];
@@ -26,12 +28,12 @@ $(document).ajaxComplete(function() {
         
         //If our headline contains "bad" words...
         for (i=0; i<badwords.length; i++) {
-        	badword = badwords[i].toLowerCase();
+            badword = badwords[i].toLowerCase();
             
-    		if (headline.indexOf(badword) >= 0) {
+            if (headline.indexOf(badword) >= 0) {
                 numremoved++;
                 
-            	cell.remove();   
+                cell.remove();   
             }
         }
     });
