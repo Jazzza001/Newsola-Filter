a2a517c8e0c44f8b6ef35d6225f009a13632ff52
Jared Williams <jared@Jareds-iMac.local>
2013-11-04 14:51:55 +0800

    Now using local storage
    Bad words are now stored in the browser's local storage. In preperation
    for user editable bad words.

--- Newsola Filter.user.js
+++ Newsola Filter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		Newsola Filter
 // @namespace	http://www.jaredwilliams.com.au
-// @version		0.1
+// @version		0.1.1
 // @description	Filters headlines from Newsola to suit what you do or do not want to read.
 // @include		*newsola.com*
 // @exclude		*newsola.com/get*
@@ -10,12 +10,73 @@
 
 
 console.log('Running Newsola Filter userscript');
+
+
+function saveBadWord(Word) {
+    var current = [];
+    
+    //Our current words...
+    current = getBadWords();
+    
+    //Append...
+    current[current.length] = Word;
+    
+    //Save...
+    saveBadWords(current);
+    
+    console.log('Bad word "' + Word + '" saved');
+}
+
+
+function saveDefaultWords() {
+    var defaults = [];
+    
+    //Our defaults...
+    defaults = ['dead', 'killed', 'murder', 'death', 'shoot', 'fire'];
+    
+    for (i=0; i<defaults.length; i++) {
+    	saveBadWords(defaults);
+    }
+    
+    console.log('Default words saved');
+}
+
+
+function saveBadWords(Words) {
+    var encoded = '';
+    
+    //Encode...
+    encoded = JSON.stringify(Words);
+    
+    //Save...
+    localStorage.setItem('badwords', encoded);
+}
+
+
+function loadBadWords() {
+	var words = '';
+    
+    //Retrieve...
+    words = localStorage.getItem('badwords');
+
+    //Parse...
+    if (words) {
+        words = JSON.parse(words);
+    } else {
+       	saveDefaultWords();
+        
+    	words = loadBadWords();
+    }
+    
+    return words;
+}
+
     
 $(document).ajaxComplete(function() {
     var cells, badwords, badword, numremoved = 0;
     
     //Our list of words to remove headlines...
-    badwords = ['dead', 'killed', 'murder', 'death', 'shoot', 'fire'];
+    badwords = loadBadWords();
     
     //Get each cell...
     cells = $('A.block');
